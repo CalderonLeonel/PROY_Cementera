4f6530cd1ee30e21d24ad8e1638866ebe1e8f247
SebastianClavijoA <cag5000404@est.univalle.edu>
2023-08-21 01:12:43 -0400

    Creacion vista adquisicion

---
+++ src/components/Adquisicion.vue
@@ -0,0 +1,342 @@
+<template>
+   <v-card elevation="5" outlined>
+        <div>
+            <v-alert dense style="color: #ffffff;" color="grey">
+                <h3>Cotizaciones</h3>
+            </v-alert>
+        </div>
+        <div>
+            <v-form ref="form" v-model="valid" lazy-validation>
+                <v-container>
+                    <v-row>
+                        <v-col cols="12" md="4">
+                            <v-btn color="success" @click="showModalAgregarCotizacionAdquisicion()">NUEVA COTIZACION DE ADQUISICIONES</v-btn>
+                        </v-col>
+                        <v-col cols="12">
+                            <v-list-item>
+                                <v-list-item-title class="text-center">
+                                    <h3>Cotizaciones de adquisiciones</h3>
+                                </v-list-item-title>
+                            </v-list-item>
+
+                            <v-card-title>
+                               <v-text-field v-model="searchCotizacionAdquisicion" append-icon="mdi-magnify" label="Buscar Almacen"
+                                    single-line hide-details></v-text-field>
+                            </v-card-title>
+
+                            <v-data-table :headers="headerCotizacionAdquisicion" :items="datosCotizacionAdquisicion" :search="searchCotizacionAdquisicion"
+                                :items-per-page="5" class="elevation-1" id="tableId">
+
+                                <template #[`item.estado`]="{ item }">
+                                    <v-chip :color="getColor(item.estado)" dark>
+                                        {{ item.estado }}
+                                    </v-chip>
+                                </template>
+
+                                <template #[`item.actions`]="{ item }">
+                                    <v-icon class="mr-2" color="primary" x-large  @click="actualizarInfoCotizacionAdquisicion(item)"
+                                        title="ACTUALIZAR INFORMACION">
+                                        mdi-pencil
+                                    </v-icon>
+                                    <v-icon v-if="item.estado == 'INACTIVO'" x-large color="success" class="mr-2" @click="activar(item)"
+                                        title="ACTIVAR COTIZACION">
+                                        mdi-check-circle-outline
+                                    </v-icon>
+                                    <v-icon v-if="item.estado == 'ACTIVO'" x-large color="error" class="mr-2" @click="desactivar(item)"
+                                        title="DESACTIVAR COTIZACION">
+                                        mdi-close-circle
+                                    </v-icon>             
+                                </template>
+
+                              
+
+
+                            </v-data-table>
+                        </v-col>
+                    </v-row>
+                    <v-row>
+                        <v-col cols="12" md="4">
+                            <v-btn color="success" @click="showModalAgregarCotizacionItem()">NUEVA COTIZACION DE ITEM</v-btn>
+                        </v-col>
+                        <v-col cols="12">
+                            <v-list-item>
+                                <v-list-item-title class="text-center">
+                                    <h3>Cotizacion Items</h3>
+                                </v-list-item-title>
+                            </v-list-item>
+
+                            <v-card-title>
+                               <v-text-field v-model="searchCotizacionItem" append-icon="mdi-magnify" label="Buscar Seccion"
+                                    single-line hide-details></v-text-field>
+                            </v-card-title>
+
+                            <v-data-table :headers="headerCotizacionItem" :items="datosCotizacionItem" :search="searchCotizacionItem"
+                                :items-per-page="5" class="elevation-1" id="tableId">
+
+                                <template #[`item.estado`]="{ item }">
+                                    <v-chip :color="getColor(item.estado)" dark>
+                                        {{ item.estado }}
+                                    </v-chip>
+                                </template>
+
+                                <template #[`item.actions`]="{ item }">
+                                    <v-icon class="mr-2" color="primary" x-large  @click="actualizarInfoCotizacionItem(item)"
+                                        title="ACTUALIZAR INFORMACION">
+                                        mdi-pencil
+                                    </v-icon>
+                                    <v-icon v-if="item.estado == 'INACTIVO'" x-large color="success" class="mr-2" @click="activar(item)"
+                                        title="ACTIVAR COTIZACION">
+                                        mdi-check-circle-outline
+                                    </v-icon>
+                                    <v-icon v-if="item.estado == 'ACTIVO'" x-large color="error" class="mr-2" @click="desactivar(item)"
+                                        title="DESACTIVAR COTIZACION">
+                                        mdi-close-circle
+                                    </v-icon>             
+                                </template>
+
+                              
+
+
+                            </v-data-table>
+                        </v-col>
+                    </v-row>
+                    
+
+                </v-container>
+            </v-form>
+
+        </div>
+
+    </v-card>
+    
+
+</template>
+<script>
+import axios from "axios";
+export default {
+    data() {
+        return {
+            //#region Proveedor
+            idProveedor: "",
+            nombreProveedor: "",
+            contactoProveedorPrincipal: "",
+            contactoProveedorecundario: "",
+            correoProveedor: "",
+            //fechaDeModificacion: "",
+            valid: true,
+            nombreRules: [
+              (v) => !!v || "Se requiere el nombre del proveedor.",
+              (v) =>
+              (v && v.length <= 60) ||
+                "el nombre del proveedor no debe sobrepasar los 60 caracteres.",
+            ],
+            phone1Rules: [
+              (v) => !!v || "Se requiere un numero telefonico o celular.",
+              (v) =>
+              (v && v.length <= 10) ||
+                "El telephono principal debe tener hasta 10 caracteres.",
+            ],
+            phone2Rules: [
+              (v) =>
+              (v && v===null || v.length <= 10) ||
+                "El telephono secundario debe tener hasta 10 caracteres.",
+            ],
+            emailRules: [
+              (v) => !!v || "Se requiere el correo electronico del proveedor",
+              (v) => /.+@.+\..+/.test(v) || "Debe ser un correo electronico valido",
+             ],
+            datosCotizacionAdquisicion: [],
+            headerCotizacionAdquisicion: [
+                { text: "COTIZACIÓN", value: "idcotizacion", sortable: true },
+                { text: "EMPLEADO", value: "idusuario", sortable: true },
+                { text: "PROVEEDOR", value: "idproveedor", sortable: true },
+                { text: "NOMBRE COTIZACIÓN", value: "nombrecotizacion", sortable: true },
+                { text: "ESTADO", value: "estado", sortable: true },
+                { text: "ACCIONES", value: "actions", sortable: false }
+                //{ text: "FECHA MODIFICACION", value: "fechmod", sortable: false },
+            ],
+
+            datosCotizacionItem: [],
+            headerCotizacionItem: [
+                { text: "COTIZACIÓN ITEM", value: "idcotizacionitem", sortable: true },
+                { text: "COTIZACIÓN", value: "idcotizacion", sortable: true },
+                { text: "ITEM", value: "iditem", sortable: true },
+                { text: "PRECIO UNITARIO", value: "preciounitario", sortable: true },
+                { text: "ESTADO", value: "estado", sortable: true },
+                { text: "ACCIONES", value: "actions", sortable: false }
+                //{ text: "FECHA MODIFICACION", value: "fechmod", sortable: false },
+            ],
+
+
+            searchCotizacionAdquisicion: "",
+            agregarCotizacionAdquisicionModal: false,
+
+            searchCotizacionItem: "",
+            agregarCotizacionItemModal: false,
+            //#endregion
+        }
+    },
+    created: function (){
+      this.listarCotizacionAdquisicion();
+      this.listarCotizacion();
+    },
+    methods: {
+        getColor(est) {
+            if (est == "ACTIVO") return 'green'
+            else if (est == "INACTIVO") return 'red'
+
+        },
+        //#region Listar
+        listarCotizacionAdquisicion() {
+            this.listarCotizacionesAdquisicion();
+        },
+        async listarCotizacionesAdquisicion() {
+          let me = this;
+          await axios
+            .get("/adquisicion/listarcotizaciondeadquisicion/")
+            .then(function (response) {
+              if (response.data.resultado == null) {
+                me.datosCotizacionAdquisicion = [];
+                console.log(response.data);
+              } else {
+                console.log(response.data);
+                me.datosCotizacionAdquisicion = response.data.resultado;
+              }
+            })
+            .catch(function (error) {
+              console.log(error);
+            });
+        },
+
+
+        listarCotizacionItem() {
+            this.listarCotizacionesItem();
+        },
+        async listarCotizacionesItem() {
+          let me = this;
+          await axios
+            .get("/adquisicion/listarcotizacionitem/")
+            .then(function (response) {
+              if (response.data.resultado == null) {
+                me.datosCotizacionItem = [];
+                console.log(response.data);
+              } else {
+                console.log(response.data);
+                me.datosCotizacionItem = response.data.resultado;
+              }
+            })
+            .catch(function (error) {
+              console.log(error);
+            });
+        },
+
+
+       
+        activar(item) {
+            this.idProveedor = item.idProveedor;
+            this.activarmateria(this.idProveedor);
+        },
+        async activarmateria(idProveedor) {
+            let me = this;
+            /*await axios
+                .post("/carrera/onmateria/" + this.idProveedor).then(function (response) {
+
+                    me.listarMaterias();
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });*/
+
+        },
+        desactivar(item) {
+            this.idProveedor = item.idProveedor;
+            this.desactivarproveedor(this.idProveedor);
+        },
+        async desactivarproveedor(idProveedor) {
+            let me = this;
+            /*await axios
+                .post("/carrera/offmateria/" + this.idProveedor).then(function (response) {
+
+                    me.listarMaterias();
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });*/
+
+        },
+        
+        llenarCamposProveedores(item) {
+            this.botonEst = 1;
+            this.nombreMateria = item.nom;
+            this.codigoMateria = item.codmat;
+            this.idMateria = item.idmateria;
+
+        },
+        //#endregion
+        //#region Adicionar
+        //#endregion
+        //#region Editar
+        //#endregion
+        //#region Eliminar
+        //#region Modals
+        editItem (item) {
+        this.editedIndex = this.desserts.indexOf(item)
+        this.editedItem = Object.assign({}, item)
+        this.dialog = true
+        },
+
+        deleteItem (item) {
+            this.editedIndex = this.desserts.indexOf(item)
+            this.editedItem = Object.assign({}, item)
+            this.dialogDelete = true
+        },
+
+        deleteItemConfirm () {
+            this.desserts.splice(this.editedIndex, 1)
+            this.closeDelete()
+        },
+
+        close () {
+            this.dialog = false
+            this.$nextTick(() => {
+            this.editedItem = Object.assign({}, this.defaultItem)
+            this.editedIndex = -1
+            })
+        },
+
+        closeDelete () {
+            this.dialogDelete = false
+            this.$nextTick(() => {
+            this.editedItem = Object.assign({}, this.defaultItem)
+            this.editedIndex = -1
+            })
+        },
+
+        save () {
+            if (this.editedIndex > -1) {
+            Object.assign(this.desserts[this.editedIndex], this.editedItem)
+            } else {
+            this.desserts.push(this.editedItem)
+            }
+            this.close()
+        },
+
+
+
+        showModalAgregarProveedor() {
+            this.agregarProveedorModal = true;
+        },
+        closeModalAgregarProveedor() {
+            this.agregarProveedorModal = false;
+        },
+        showFormato() {
+            this.formatoModal = true;
+        },
+        closeFormato() {
+            this.formatoModal = false;
+        },
+        //#endregion
+      },
+};
+
+</script>
