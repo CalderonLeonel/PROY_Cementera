3a6f948294ba6ee66622810c35ce167622b7aed0
SebastianClavijoA <cag5000404@est.univalle.edu>
2023-08-30 23:25:16 -0400

    CRUD completo de proveedor

--- src/components/Proveedor.vue
+++ src/components/Proveedor.vue
@@ -2,7 +2,7 @@
    <v-card elevation="5" outlined>
         <div>
             <v-alert dense style="color: #ffffff;" color="grey">
-                <h5>Proveedores</h5>
+                <h5>llenarCamposProveedores</h5>
             </v-alert>
         </div>
         <div>
@@ -10,7 +10,7 @@
                 <v-container>
                     <v-row>
                         <v-col cols="12" md="4">
-                            <v-btn color="success" @click="showModalAgregarProveedor()">NUEVO Proveedor</v-btn>
+                            <v-btn color="success" @click="showModalAgregarProveedor()">NUEVO PROVEEDOR</v-btn>
                         </v-col>
                         <v-col cols="12">
                             <v-list-item>
@@ -34,7 +34,7 @@
                                 </template>
 
                                 <template #[`item.actions`]="{ item }">
-                                    <v-icon class="mr-2" color="primary" x-large  @click="actualizarInfoProveedor(item)"
+                                    <v-icon class="mr-2" color="primary" x-large  @click="llenarCamposProveedor(item)"
                                         title="ACTUALIZAR INFORMACION">
                                         mdi-pencil
                                     </v-icon>
@@ -42,7 +42,7 @@
                                         title="ACTIVAR PROVEEDOR">
                                         mdi-check-circle-outline
                                     </v-icon>
-                                    <v-icon v-if="item.est == 'ACTIVO'" x-large color="error" class="mr-2" @click="desactivar(item)"
+                                    <v-icon v-if="item.est == 'ACTIVO'" x-large color="error" class="mr-2" @click="confirmacionAnulacion(item)"
                                         title="DESACTIVAR PROVEEDOR">
                                         mdi-close-circle
                                     </v-icon>             
@@ -64,22 +64,94 @@
                 <v-card-title>
                     <span>Agregar Proveedor</span>
                 </v-card-title>
-            </v-card>
-        </v-dialog>
+                <v-card-text>
+                    <v-form ref="form" v-model="valid" lazy-validation>
+                        <v-container>
+                            <v-row>
+                                <v-col cols="12" md="4">
+                                    <v-text-field v-model="nombreProveedor" label="NOMBRE PROVEEDOR" :counter="60"
+                                        :rules="nombreRules" @input="nombreProveedor = nombreProveedor.toUpperCase()"
+                                        required></v-text-field>
+                                </v-col>
+                                <v-col cols="12" md="4">
+                                    <v-text-field v-model="contactoProveedorPrincipal" type="number" label="CONTACTO" :counter="10"
+                                        :rules="phone1Rules" @input="contactoProveedorPrincipal = contactoProveedorPrincipal.toUpperCase()"
+                                        required></v-text-field>
+                                </v-col>
+                                <v-col cols="12" md="4">
+                                    <v-text-field v-model="contactoProveedorecundario" type="number" label="CONTACTO SECUNDARIO (OPCIONAL)" :counter="10"
+                                        :rules="phone2Rules" @input="contactoProveedorecundario = contactoProveedorecundario.toUpperCase()"
+                                        required></v-text-field>
+                                </v-col>
+                                <v-col cols="12" md="4">
+                                    <v-text-field v-model="correoProveedor" type="email" label="CORREO" :counter="100"
+                                        :rules="emailRules" @input="correoProveedor = correoProveedor"
+                                        required></v-text-field>
+                                </v-col>   
+                                <v-col cols="12" md="4"> </v-col>
+                                <v-col cols="6"></v-col>
+                                <v-col cols="2">
+                                    <v-btn iconvv v-if="botonAct == 1" class="mx-4"  dark color="#0A62BF"
+                                            @click="editarProv()" style="float: left"
+                                            title="ACTUALIZAR INFORMACIÓN">
+                                            <v-icon dark> mdi-pencil </v-icon>
+                                            ACTUALIZAR
+                                        </v-btn>
+                                        <v-btn iconv v-if="botonAct == 0" class="mx-4"  dark color="#0ABF55"
+                                            @click="registrarProv()" style="float: left" title="REGISTRAR PROVEEDOR">
+                                            <v-icon dark> mdi-content-save </v-icon>
+                                            GUARDAR
+                                        </v-btn>
+                                </v-col>                      
+                                <v-col cols="2">                                        
+                                    <v-btn iconv color="#BF120A" class="mx-4"  dark  @click="limpiar()"
+                                        style="float: left" title="LIMPIAR FORMULARIO">
+                                        <v-icon dark> mdi-eraser </v-icon>
+                                        LIMPIAR
+                                    </v-btn>
+                                </v-col>
+                                <v-col cols="2">
+                                    <v-btn class="mx-2" iconv dark color="#00A1B1"
+                                        @click="closeModalAgregarProveedor()" style="float: right" title="SALIR">
+                                        <v-icon dark> mdi-close-circle-outline </v-icon>
+                                        SALIR
+                                    </v-btn>
+                                </v-col>
+                            </v-row>
+                        </v-container>
+                    </v-form>
 
-        <v-dialog v-model="editarProductoModal" max-width="1000px">
-            <v-card elevation="5" outlined>
-                <v-card-title>
-                    <span>Editar Proveedor</span>
-                </v-card-title>
+                </v-card-text>
             </v-card>
         </v-dialog>
 
         <v-dialog v-model="confirmacionAnulacionProveedor" max-width="1000px">
             <v-card elevation="5" outlined>
                 <v-card-title>
-                    <span>Confirmación de Eliminacion</span>
+                    <span>¿ESTAS SEGURO?</span>
                 </v-card-title>
+                <v-card-text>
+                    <v-container>
+                            <v-row>
+                                <v-col cols="3"></v-col>
+                                <v-col cols="3">
+                                    <v-btn class="mx-2"  dark x-big color="#BF120A"
+                                        @click="anular()" style="float: right" title="ANULAR">
+                                        <v-icon dark> mdi-close-circle-outline </v-icon>
+                                        ANULAR
+                                    </v-btn>
+                                </v-col>
+                                <v-col cols="3">
+                                    <v-btn class="mx-2"  dark x-big color="#00A1B1"
+                                        @click="closeAnulacion()" style="float: right" title="SALIR">
+                                        <v-icon dark> mdi-close-circle-outline </v-icon>
+                                        SALIR
+                                    </v-btn>
+                                </v-col>
+                                <v-col cols="3"></v-col>
+                            </v-row>
+                    </v-container>
+                </v-card-text>
             </v-card>
         </v-dialog>
     </v-card>
@@ -97,6 +169,7 @@ export default {
             contactoProveedorPrincipal: "",
             contactoProveedorecundario: "",
             correoProveedor: "",
+            estado:"ACTIVO",
             //fechaDeModificacion: "",
             valid: true,
             nombreRules: [
@@ -106,14 +179,15 @@ export default {
                 "el nombre del proveedor no debe sobrepasar los 60 caracteres.",
             ],
             phone1Rules: [
-              (v) => !!v || "Se requiere un numero telefonico o celular.",
+                (v) => !!v ||"Se requiere un numero telefonico o celular.",
+               (v) => v.length >= 7 || "El telephono principal debe tener al menos 7 caracteres.",
               (v) =>
-              (v && v.length <= 10) ||
+              (v  && v.length <= 10) ||
                 "El telephono principal debe tener hasta 10 caracteres.",
             ],
             phone2Rules: [
               (v) =>
-              (v && v===null || v.length <= 10) ||
+              (v && v===null || v.length >= 7 || v.length <= 10) ||
                 "El telephono secundario debe tener hasta 10 caracteres.",
             ],
             emailRules: [
@@ -134,6 +208,8 @@ export default {
 
             searchProveedor: "",
             agregarProveedorModal: false,
+            confirmacionAnulacionProveedor: false,
+            botonAct: 0,
             //#endregion
         }
     },
@@ -153,7 +229,7 @@ export default {
         async listarProveedores() {
           let me = this;
           await axios
-            .get("/proveedor/listarproveedores/")
+            .get("/proveedor/listarproveedoresactivos/")
             .then(function (response) {
               if (response.data.resultado == null) {
                 me.datosProveedor = [];
@@ -169,44 +245,129 @@ export default {
         },
         activar(item) {
             this.idProveedor = item.idProveedor;
-            this.activarmateria(this.idProveedor);
+            this.activarproveedor(this.idProveedor);
         },
-        async activarmateria(idProveedor) {
+        async activarproveedor(idProveedor) {
             let me = this;
             /*await axios
-                .post("/carrera/onmateria/" + this.idProveedor).then(function (response) {
+                .post("/proveedor/activar/" + this.idProveedor).then(function (response) {
 
-                    me.listarMaterias();
+                    me.listarProveedor();
                 })
                 .catch(function (error) {
                     console.log(error);
                 });*/
 
         },
-        desactivar(item) {
-            this.idProveedor = item.idProveedor;
+
+        registrarProv() {
+            this.registrarProveedor(this.nombreProveedor, this.contactoProveedorPrincipal, this.contactoProveedorecundario,this.correoProveedor,this.estado);
+            this.closeModalAgregarProveedor();
+        },
+        async registrarProveedor(
+            nombreProveedor,
+            contactoProveedorPrincipal,
+            contactoProveedorecundario,
+            correoProveedor,
+            estado
+        ) {
+            let me = this;
+            await axios
+                .post(
+                    "/proveedor/insertar/" +
+                    this.nombreProveedor +
+                    "," +
+                    this.contactoProveedorPrincipal +
+                    "," +
+                    this.contactoProveedorecundario +
+                    "," +
+                    this.correoProveedor +
+                    "," +
+                    this.estado
+                )
+                .then(function (response) {
+
+                    me.mensajeSnackbar = response.data.message;
+                    me.snackbarOK = true;
+                    me.limpiar();
+                    me.listarProveedores();
+                })
+                .catch(function (error) {
+                    me.snackbarError = true;
+
+                });
+
+        },
+
+        editarProv() {
+            this.botonAct = 0;
+
+            this.editarProveedor(this.idProveedor,this.nombreProveedor, this.contactoProveedorPrincipal, this.contactoProveedorecundario,this.correoProveedor,this.estado);
+            this.closeModalAgregarProveedor();
+        },
+        async editarProveedor(
+            idProveedor,
+            nombreProveedor,
+            contactoProveedorPrincipal,
+            contactoProveedorecundario,
+            correoProveedor,
+            estado
+        ) {
+            let me = this;
+            await axios
+                .post(
+                    "/proveedor/editar/" +
+                    this.idProveedor +
+                    "," +
+                    this.nombreProveedor +
+                    "," +
+                    this.contactoProveedorPrincipal +
+                    "," +
+                    this.contactoProveedorecundario +
+                    "," +
+                    this.correoProveedor +
+                    "," +
+                    this.estado
+                )
+                .then(function (response) {
+
+                    me.mensajeSnackbar = response.data.message;
+                    me.snackbarOK = true;
+                    me.limpiar();
+                    me.listarProveedores();
+                })
+                .catch(function (error) {
+                    me.snackbarError = true;
+                    alert('error');
+                });
+
+        },
+        confirmacionAnulacion(item){
+            this.idProveedor = item.idprv;
+            this.confirmacionAnulacionProveedor = true;
+        },
+        closeAnulacion(){
+            this.confirmacionAnulacionProveedor = false;
+        },
+        anular() {
             this.desactivarproveedor(this.idProveedor);
+            this.confirmacionAnulacionProveedor = false;
         },
         async desactivarproveedor(idProveedor) {
             let me = this;
-            /*await axios
-                .post("/carrera/offmateria/" + this.idProveedor).then(function (response) {
+            await axios
+                .post("/proveedor/anular/" + this.idProveedor).then(function (response) {
 
-                    me.listarMaterias();
+                    me.listarProveedores();
                 })
                 .catch(function (error) {
                     console.log(error);
-                });*/
+                    alert('error')
+                });
 
         },
         
-        llenarCamposProveedores(item) {
-            this.botonEst = 1;
-            this.nombreMateria = item.nom;
-            this.codigoMateria = item.codmat;
-            this.idMateria = item.idmateria;
 
-        },
         //#endregion
         //#region Adicionar
         //#endregion
@@ -256,19 +417,29 @@ export default {
             this.close()
         },
 
-
-
-        showModalAgregarProveedor() {
+        llenarCamposProveedor(item) {
+            this.botonAct = 1;
             this.agregarProveedorModal = true;
+            this.idProveedor = item.idprv;
+            this.nombreProveedor = item.nomprv;
+            this.contactoProveedorPrincipal = item.cto1pro;
+            this.contactoProveedorecundario = item.cto2pro;
+            this.correoProveedor = item.croprov;
+            this.estado = item.est;
+        },
+
+
+        showModalAgregarProveedor(item) {
+            this.agregarProveedorModal = true;
+            botonAct = 0;
         },
         closeModalAgregarProveedor() {
             this.agregarProveedorModal = false;
+            this.limpiar();
         },
-        showFormato() {
-            this.formatoModal = true;
-        },
-        closeFormato() {
-            this.formatoModal = false;
+
+        limpiar () {
+            this.$refs.form.reset()
         },
         //#endregion
       },
