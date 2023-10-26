598099e3e62b843220bcda11bfe8103018625051
SebastianClavijoA <90160539+SebastianClavijoA@users.noreply.github.com>
2023-10-26 08:38:04 -0400

    Actualizando ip del servidor

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "http://10.128.0.2:3000",
   headers: {
     "Content-type": "application/json"
   }
