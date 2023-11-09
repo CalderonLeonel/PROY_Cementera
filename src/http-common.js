f853e5c0adc0dacb6d58ef9312b1528de9adc20d
SebastianClavijoA <90160539+SebastianClavijoA@users.noreply.github.com>
2023-11-09 09:44:21 -0400

    Cambiando la direccion ip

--- src/http-common.js
+++ src/http-common.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default axios.create({
   //baseURL: "http://35.202.76.61:3000",
-  baseURL: "http://10.128.0.2:3000",
+  baseURL: "http://35.222.66.155:3000",
   headers: {
     "Content-type": "application/json"
   }
