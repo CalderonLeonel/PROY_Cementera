e5c25ca58715e452abe11d8c5afeb94b1d5fb85d
SebastianClavijoA <cag5000404@est.univalle.edu>
2024-11-25 16:24:28 -0400

    Fix bugs on registroVentas

--- src/components/Ventas.vue
+++ src/components/Ventas.vue
@@ -562,41 +562,46 @@ export default {
                     this.idCliente + "," +
                     this.venta.idEmpleado
                 );
+                if (response.status == 200 ||  response.data.success) {
+                        this.recuperarUltimaVenta().then(() => {
+                            this.datosUltimaVenta.forEach((venta, index) => {
+                                this.idUltimaVenta = venta.idven;
+                                this.fechaVenta = venta.creadate;
+                            });
+                        this.imprimirFactura(this.nit, this.razonSocial, this.idUltimaVenta, this.fechaVenta);
+
+                        this.imprimirRecibo(this.razonSocial, this.idUltimaVenta, this.fechaVenta);
+                    })
+                }
+                
 
                 if (response.data && response.data.success) {
                     this.mensajeSnackbar = response.data.message || "Venta registrada exitosamente";
+                    console.log('snackbar positivo');
                     this.snackbarOK = true;
                     this.snackbarError = false;
                     // Recuperar la última venta
-                    me.recuperarUltimaVenta().then(() => {
-                        this.datosUltimaVenta.forEach((venta, index) => {
-                            console.log(`Venta ${index + 1}:`);
-                            this.idUltimaVenta = venta.idven;
-                            this.fechaVenta = venta.creadate;
-                        });
-                        me.imprimirFactura(nit, razonSocial, me.idUltimaVenta, me.fechaVenta);
+                    console.log("recuperar ultima venta")
+                    this.recuperarUltimaVenta();
+                    
 
-                        me.imprimirRecibo(razonSocial, me.idUltimaVenta, me.fechaVenta);
-                    });
-                    // Registrar productos del carrito
-                    me.registrarVentasCarrito();
-                    //Registrar asiento contable
-                    me.registrarAsientosContables();
-
-                    await this.recuperarUltimaVenta();
-                    await this.registrarVentasCarrito();
-                    await this.registrarAsientosContables();
+                    this.recuperarUltimaVenta();
+                     this.registrarVentasCarrito();
+                     this.registrarAsientosContables();
 
                     this.resetVenta();
-                    //Limpiamos el formulario
-                    me.resetVenta();
 
 
                 } else {
+                    console.log('snackbar error');
+                    console.log(response.data.Error);
+                    this.mensajeSnackbarError = response.data.message;
                     throw new Error(response.data.message || "Error desconocido al registrar la venta");
                 }
             } catch (error) {
                 console.error("Error al registrar la venta:", error);
+                console.log(response.data.message);
+                console.log(error.response?.data?.message );
                 this.mensajeSnackbarError = error.response?.data?.message || error.message || "Error al registrar la venta";
                 this.snackbarError = true;
                 this.snackbarOK = false;
