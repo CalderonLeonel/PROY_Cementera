d95c83acee1d0396514b9fe78be0958a5470d4bd
Brandon Montoya <MGJ2019761@est.univalle.edu>
2024-11-30 21:40:37 -0400

    Anulacion de Facturas

--- src/components/RegistroVentas.vue
+++ src/components/RegistroVentas.vue
@@ -1,33 +1,65 @@
 <template>
 
-<v-card elevation="5" outlined v-if=" checkAccess(3, 'SUPERVISOR' ) || checkAccess(3, 'GERENTE')">
+    <v-card elevation="5" outlined>
+
+        <v-dialog v-model="anularModal" max-width="500px">
+            <v-card>
+                <v-card-title>Anular Factura</v-card-title>
+                <v-card-text>
+                    <v-textarea v-model="motivoAnulacion" label="Motivo de anulación" required></v-textarea>
+                </v-card-text>
+                <v-card-actions>
+                    <v-btn color="red" @click="anularFactura">Anular</v-btn>
+                    <v-btn color="grey" @click="dialogoAnulacion = false">Cancelar</v-btn>
+                </v-card-actions>
+            </v-card>
+        </v-dialog>
+
         <div>
             <v-alert dense style="color: #ffffff;" color="blue">
                 <h3>REGISTROS DE VENTAS</h3>
             </v-alert>
         </div>
         <v-container>
-            <v-row >
-                <v-col cols="12" md="12" >
+            <v-row>
+                <v-col cols="12" md="12">
                     <v-text-field v-model="searchVenta" append-icon="mdi-magnify" label="BUSCAR VENTA" single-line
                         hide-details></v-text-field>
                     <v-data-table :headers="headerVenta" :items="datosVenta" :search="searchVenta"
                         :custom-filter="customFilter" class="elevation-1">
                         <template #[`item.actions`]="{ item }">
                             <v-btn color="primary" small @click="imprimirFactura(item)">IMPRIMIR FACTURA</v-btn>
-                                            
+
                             <v-btn color="secondary" small @click="imprimirRecibo(item)">IMPRIMIR RECIBO</v-btn>
+                            <v-btn color="error" small @click="openAnularModal(item)">
+                                ANULAR FACTURA
+                            </v-btn>
                         </template>
-                      
+
                     </v-data-table>
                 </v-col>
             </v-row>
         </v-container>
-       
+
 
         <div class="text-center">
-            <v-snackbar v-model="snackbarError" :timeout="timeout" top right shaped dense color="error" outlined>
+            <v-snackbar v-model="snackbarOK" :timeout="timeout" top right shaped dense color="#00FF00" outlined>
+                <strong>{{ mensajeSnackbar }}</strong>
+
+
+                <template v-slot:action="{ attrs }">
+                    <v-icon right v-bind="attrs" @click="snackbarOK = false">
+                        mdi-close
+                    </v-icon>
+                </template>
+            </v-snackbar>
+        </div>
+
+        <div class="text-center">
+
+            <v-snackbar v-model="snackbarError" :timeout="timeout" top right shaped dense color="#EE680B" outlined>
                 <strong>{{ mensajeSnackbarError }}</strong>
+
                 <template v-slot:action="{ attrs }">
                     <v-icon right v-bind="attrs" @click="snackbarError = false">
                         mdi-close
@@ -50,8 +82,8 @@ export default {
             user: { id_usuario: 0, usuario: '', accesos: [], tipo: '', nombres: '', paterno: '', materno: '' },
 
 
-            datosVenta: [], 
-            headerVenta:  [
+            datosVenta: [],
+            headerVenta: [
                 { text: "NÚMERO DE VENTA", value: "idven", sortable: true },
                 { text: "TOTAL", value: "tot", sortable: true },
                 { text: "CÓDIGO DE CONTROL", value: "codctrl", sortable: true },
@@ -63,15 +95,16 @@ export default {
             searchVenta: "",
 
             datosDetalleVenta: [],
-            headerDetalleVenta:  [
+            headerDetalleVenta: [
                 { text: "NOMPRE DE PRODCUTO", value: "nomprod", sortable: true },
                 { text: "CANTIDAD", value: "cant", sortable: true },
                 { text: "PRECIO UNITARIO", value: "precuni", sortable: true },
             ],
 
             searchProducto: "",
+            anularModal: 0,
 
-           
+
         }
     },
     created: function () {
@@ -82,7 +115,7 @@ export default {
         }
         if (this.user == null) {
             if (this.$route.path != '/login') {
-            this.$router.push("/login");
+                this.$router.push("/login");
             }
         }
     },
@@ -92,8 +125,8 @@ export default {
                 this.user = JSON.parse(sessionStorage.getItem('session'));
             }
             return this.user;
-            }
-        }, 
+        }
+    },
     methods: {
 
         getDate() {
@@ -135,18 +168,18 @@ export default {
         numberToLetters(num) {
             const unidades = ["", "UNO", "DOS", "TRES", "CUATRO", "CINCO", "SEIS", "SIETE", "OCHO", "NUEVE"];
             const decenas = [
-                "", "DIEZ", "VEINTE", "TREINTA", "CUARENTA", "CINCUENTA", 
+                "", "DIEZ", "VEINTE", "TREINTA", "CUARENTA", "CINCUENTA",
                 "SESENTA", "SETENTA", "OCHENTA", "NOVENTA"
             ];
             const especiales = [
-                "DIEZ", "ONCE", "DOCE", "TRECE", "CATORCE", "QUINCE", 
+                "DIEZ", "ONCE", "DOCE", "TRECE", "CATORCE", "QUINCE",
                 "DIECISÉIS", "DIECISIETE", "DIECIOCHO", "DIECINUEVE"
             ];
             const centenas = [
-                "", "CIENTO", "DOSCIENTOS", "TRESCIENTOS", "CUATROCIENTOS", 
+                "", "CIENTO", "DOSCIENTOS", "TRESCIENTOS", "CUATROCIENTOS",
                 "QUINIENTOS", "SEISCIENTOS", "SETECIENTOS", "OCHOCIENTOS", "NOVECIENTOS"
             ];
-            
+
             // Agregar la palabra "mil" al proceso
             const miles = Math.floor(num / 1000);
             num = num % 1000;
@@ -248,14 +281,14 @@ export default {
             try {
                 const response = await axios.get(`/venta/listardetalle/` + item.idven);
                 const jsonData = response.data.resultado || [];
-                
+
                 var total = 0
                 jsonData.forEach(detalle => {
-                total += detalle.cant * detalle.precuni;
+                    total += detalle.cant * detalle.precuni;
                 });
-               
 
-                const bodyData = jsonData.map(data => [ 
+
+                const bodyData = jsonData.map(data => [
                     data.nomprod,
                     data.cant,
                     data.precuni
@@ -272,15 +305,15 @@ export default {
                 doc.text(`Factura`, 105, 60, { align: "center" });
                 doc.text(`N°: ${item.idven}`, 105, 70, { align: "center" });
 
-                doc.text(`Fecha: ${ this.getFormattedDate(item.creadate)}`, 105, 80, { align: "center" });
+                doc.text(`Fecha: ${this.getFormattedDate(item.creadate)}`, 105, 80, { align: "center" });
                 doc.text(`NIT/CI Cliente: ${item.nit}`, 105, 90, { align: "center" });
                 doc.text(`NOMBRE/RAZÓN SOCIAL: ${item.razsoc}`, 105, 100, { align: "center" });
 
                 doc.text(`DETALLE`, 105, 110, { align: "center" })
                 doc.setFontSize(9);
-                let startY = 130;   
+                let startY = 130;
                 doc.autoTable({
-                    startY: startY, 
+                    startY: startY,
                     styles: {
                         fillColor: [255, 255, 255], // Fondo blanco
                         textColor: [0, 0, 0],      // Texto negro
@@ -296,38 +329,38 @@ export default {
                     head: [["PRODUCTO", "CANTIDAD", "PRECIO UNITARIO"]],
                     body: bodyData
                 });
-                                //doc.autoTable({ head: [["PRODUCTO", "CANTIDAD", "PRECIO UNITARIO",]], body: bodyData, startY: 140 });
-                    //let finalY = doc.previousAutoTable.finalY;
-                    startY += 20;
-                    doc.setFont("helvetica", "bold");
-                    doc.text("Total: "+total.toFixed(2)+" Bs.", 110, 10 + startY)
-                    doc.text("Son: "+this.transformToBolivianos(total.toFixed(2)), 120, 20 + startY )
+                //doc.autoTable({ head: [["PRODUCTO", "CANTIDAD", "PRECIO UNITARIO",]], body: bodyData, startY: 140 });
+                //let finalY = doc.previousAutoTable.finalY;
+                startY += 20;
+                doc.setFont("helvetica", "bold");
+                doc.text("Total: " + total.toFixed(2) + " Bs.", 110, 10 + startY)
+                doc.text("Son: " + this.transformToBolivianos(total.toFixed(2)), 120, 20 + startY)
 
 
-                    startY += 40;
-                    doc.setFontSize(8);
-                    doc.setFont("helvetica", "normal");
-                    doc.text("ESTA FACTURA CONTRIBUYE AL DESARROLLO DE NUESTRO PAÍS, EL USO ILÍCITO DE ÉSTA SERÁ SANCIONADO DE ACUERDO A LEY", 105, startY, { align: "center" });
-                    doc.text("Ley N° 453: Tienes derecho a recibir información sobre las características y contenidos de los servicios que utilices.", 105, startY + 10, { align: "center" });
+                startY += 40;
+                doc.setFontSize(8);
+                doc.setFont("helvetica", "normal");
+                doc.text("ESTA FACTURA CONTRIBUYE AL DESARROLLO DE NUESTRO PAÍS, EL USO ILÍCITO DE ÉSTA SERÁ SANCIONADO DE ACUERDO A LEY", 105, startY, { align: "center" });
+                doc.text("Ley N° 453: Tienes derecho a recibir información sobre las características y contenidos de los servicios que utilices.", 105, startY + 10, { align: "center" });
 
-                    doc.save("factura_"+this.getFormattedDateTime(item.creadate)+".pdf");
+                doc.save("factura_" + this.getFormattedDateTime(item.creadate) + ".pdf");
             } catch (error) {
                 console.error(error);
             }
-            },
+        },
 
         async imprimirRecibo(item) {
             try {
                 const response = await axios.get(`/venta/listardetalle/` + item.idven);
                 const jsonData = response.data.resultado || [];
-                
+
                 var total = 0
                 jsonData.forEach(detalle => {
-                total += detalle.cant * detalle.precuni;
+                    total += detalle.cant * detalle.precuni;
                 });
-               
 
-                const bodyData = jsonData.map(data => [ 
+
+                const bodyData = jsonData.map(data => [
                     data.nomprod,
                     data.cant,
                     data.precuni
@@ -340,13 +373,13 @@ export default {
                 doc.text("Drymix Bolivia SRL.", 105, 20, { align: "center" });
                 doc.setFontSize(12);
 
-                doc.text(`Fecha: ${ this.getFormattedDate(item.creadate)}`, 105, 30, { align: "center" });
+                doc.text(`Fecha: ${this.getFormattedDate(item.creadate)}`, 105, 30, { align: "center" });
                 doc.text(`NOMBRE: ${item.razsoc}`, 105, 40, { align: "center" });
                 doc.text(`DETALLE`, 105, 50, { align: "center" })
                 doc.setFontSize(9);
-                let startY = 60;   
+                let startY = 60;
                 doc.autoTable({
-                    startY: startY, 
+                    startY: startY,
                     styles: {
                         fillColor: [255, 255, 255], // Fondo blanco
                         textColor: [0, 0, 0],      // Texto negro
@@ -362,19 +395,19 @@ export default {
                     head: [["PRODUCTO", "CANTIDAD", "PRECIO UNITARIO"]],
                     body: bodyData
                 });
-                                //doc.autoTable({ head: [["PRODUCTO", "CANTIDAD", "PRECIO UNITARIO",]], body: bodyData, startY: 140 });
-                    //let finalY = doc.previousAutoTable.finalY;
-                    startY += 20;
-                    doc.setFont("helvetica", "bold");
-                    doc.text("Total: "+total.toFixed(2)+" Bs.", 105, 10 + startY)
-                    doc.text("Son: "+this.transformToBolivianos(total.toFixed(2)), 105, 20 + startY )
+                //doc.autoTable({ head: [["PRODUCTO", "CANTIDAD", "PRECIO UNITARIO",]], body: bodyData, startY: 140 });
+                //let finalY = doc.previousAutoTable.finalY;
+                startY += 20;
+                doc.setFont("helvetica", "bold");
+                doc.text("Total: " + total.toFixed(2) + " Bs.", 105, 10 + startY)
+                doc.text("Son: " + this.transformToBolivianos(total.toFixed(2)), 105, 20 + startY)
 
-            
-                    doc.save("recibo_"+this.getFormattedDateTime(item.creadate)+".pdf");
+
+                doc.save("recibo_" + this.getFormattedDateTime(item.creadate) + ".pdf");
             } catch (error) {
                 console.error(error);
             }
-            },
+        },
 
 
         checkAccess(accesoCorrecto, tipoCorrecto) {
@@ -386,26 +419,52 @@ export default {
                 let checkedType = false;
                 //Si accesoCorrecto es 0, no se requiere ningun acceso para acceder
                 if (accesoCorrecto != 0) {
-                this.user['accesos'].forEach(access => {
-                    if (access == accesoCorrecto)
-                    checkedAccess = true;
-                });
+                    this.user['accesos'].forEach(access => {
+                        if (access == accesoCorrecto)
+                            checkedAccess = true;
+                    });
                 } else checkedAccess = true;
 
                 //Si tipoCorrecto es '0', no se requiere ningun tipo de cuenta para acceder
                 if (tipoCorrecto != '0') {
-                if (this.user['tipo'] == tipoCorrecto) {
-                    checkedType = true;
-                }
+                    if (this.user['tipo'] == tipoCorrecto) {
+                        checkedType = true;
+                    }
                 } else checkedType = true;
                 if (checkedAccess && checkedType) { return true }
                 else return false;
             }
 
-            
+
 
         },
+
+        async anularFactura() {
+            if (!this.motivoAnulacion.trim()) {
+                this.mensajeSnackbarError = "El motivo de anulación es requerido.";
+                this.snackbarError = true;
+                return;
             }
+
+            try {
+                await axios.post(`/venta/offventa/` + this.ventaSeleccionada.idven + "," + this.motivoAnulacion);
+                this.dialogoAnulacion = false;
+                this.listarVentas();
+                this.$emit("factura-anulada", this.ventaSeleccionada.idven);
+                this.anularModal = false
+            } catch (error) {
+                console.error("Error anulando la factura:", error);
+                this.snackbarError = true;
+                this.mensajeSnackbarError =
+                    "Error al intentar anular la factura. Intente nuevamente.";
+            }
+        },
+
+        openAnularModal(item) {
+            this.ventaSeleccionada = item;
+            this.anularModal = true;
+        },
+    }
 
 }
 </script>
