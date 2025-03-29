93eaede3a6f05a844ea9ffb8f8158be75244ca5f
CalderonLeonel <CRL1009575@est.univalle.edu>
2025-03-28 22:32:53 -0400

    Mensaje de Home

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,16 @@
 <template>
-  <h1><p>¡Bienvenido a Dryxmix!</p></h1>
+<div>
+  <h1 style="text-align: center;">¡Bienvenido a Drymix!</h1>
+  <hr style="width: 80%; margin: 10px auto; border: 1px solid #ccc;">
+  <p style="text-align: left; margin-inline: 150px; font-size: 18px; font-family: Verdana, sans-serif">
+    Nos alegra tenerte aquí. Este sistema será tu principal herramienta de trabajo donde estarás cerca de la información y funciones esenciales para tu día a día en la empresa.
+    <br><br>
+  </p>
+  <p style="text-align: left; margin-inline: 150px; font-size: 18px; font-family: Verdana, sans-serif">
+    Por seguridad, recuerda no compartir tu contraseña con nadie. Tu acceso es personal e intransferible, y mantenerlo seguro nos ayuda a protegernos a todos.
+  </p>
+</div>
+
 
 <!--
         <v-carousel>
